65a191c123e0b2be5271ee86dd9996c695dba024
Simon Redman <simon@ergotech.com>
2023-02-25 23:06:54 -0500

    Implement basic device discovery

--- ember-mqtt-bridge.py
+++ ember-mqtt-bridge.py
@@ -10,12 +10,18 @@
 
 import asyncio
 import asyncio_mqtt
+from asyncio_mqtt import Client
 
-import ember_mug
+from ember_mug.scanner import find_mug, discover_mugs
+from ember_mug.mug import EmberMug
 
 import argparse
 from exceptiongroup import ExceptionGroup, catch
+from collections import namedtuple
+import json
 import yaml
+
+MqttPayload = namedtuple("MqttPayload", ["topic", "payload"])
 
 class EmberMqttBridge:
     def __init__(
@@ -45,6 +51,63 @@ class EmberMqttBridge:
             raise ExceptionGroup(
                 "One or more parameters was not provided",
                 [ValueError(param) for param in unsupplied_params])
+
+    async def start(self):
+        devices = await discover_mugs(self.MAC)
+        device = [device for device in devices if device.address == self.MAC][0]
+        mug = EmberMug(device)
+        async with Client(
+            hostname=self.mqtt_broker,
+            port=self.mqtt_broker_port,
+            username=self.mqtt_username,
+            password=self.mqtt_password,
+            ) as client:
+            await self.send_mqtt_discovery(client, mug)
+            await self.start_listener_loop(client, mug)
+        pass
+
+    async def send_mqtt_discovery(self, client: Client, mug: EmberMug):
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(self.send_root_device(client, mug))
+
+    def sanitise_mac(mac: str) -> str:
+        """Clean up a MAC so it's suitable for use where colons aren't"""
+        return mac.replace(":", "_")
+
+    def get_device_definition(self, mug: EmberMug):
+        return {
+            "connections": [("mac", self.MAC)],
+            "model": mug.device.name,
+            "manufacturer": "Ember",
+            "suggested_area": "Office",
+        }
+
+    async def send_root_device(self, client: Client, mug: EmberMug):
+        root_device_payload: MqttPayload = MqttPayload(
+            topic= f"{self.discovery_prefix}/climate/{EmberMqttBridge.sanitise_mac(self.MAC)}/config",
+            payload={
+                "name": mug.device.name,
+                "mode_state_topic": f"ember/{EmberMqttBridge.sanitise_mac(self.MAC)}/state",
+                "mode_state_template": "{{ value_json.power }}",
+                "current_temperature_topic": f"ember/{EmberMqttBridge.sanitise_mac(self.MAC)}/state",
+                "current_temperature_template": "{{ value_json.current_temperature }}",
+                "temperature_state_topic": f"ember/{EmberMqttBridge.sanitise_mac(self.MAC)}/state",
+                "temperature_state_template": "{{ value_json.desired_temperature }}",
+                "mode_command_topic": f"ember/{EmberMqttBridge.sanitise_mac(self.MAC)}/power/set",
+                "temperature_command_topic": f"ember/{EmberMqttBridge.sanitise_mac(self.MAC)}/temperature/set",
+                "modes": ["on", "off"],
+                #"temperature_unit": "F",
+                "temp_step": 1,
+                "unique_id": self.MAC,
+                "device": self.get_device_definition(mug),
+                "icon": "mdi:coffee",
+                #"max_temp": 120,
+                #"min_temp": 45,
+            })
+        await client.publish(root_device_payload.topic, json.dumps(root_device_payload.payload), retain=True)
+
+    async def start_listener_loop(self, client: Client, mug: EmberMug):
+        pass
 
 def main():
     parser = argparse.ArgumentParser(
@@ -92,6 +155,7 @@ def main():
     del config["config_file"]
 
     bridge = EmberMqttBridge(**config)
+    asyncio.run(bridge.start()) # Should never return
 
 if __name__ == "__main__":
     main()
