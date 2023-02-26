70c635b9f24a76183b2c784f926c50a0d3c78030
Simon Redman <simon@ergotech.com>
2023-02-25 21:05:11 -0500

    Define preliminary interface

---
+++ ember-mqtt-bridge.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+#
+# Filename: ember-mqtt-bridge.py
+#
+# Author: Simon Redman <simon@ergotech.com>
+# File Created: 25.02.2023
+# Last Modified: Sat 25 Feb 2023 08:38:58 PM EST
+# Description: 
+#
+
+import asyncio
+import asyncio_mqtt
+
+import ember_mug
+
+import argparse
+
+class EmberMqttBridge:
+    def __init__(
+        self,
+        MAC,
+        mqtt_broker,
+        mqtt_broker_port,
+        mqtt_username,
+        mqtt_password,
+        update_interval,
+        ):
+        self.MAC = MAC
+        self.mqtt_broker = mqtt_broker
+        self.mqtt_broker_port = mqtt_broker_port
+        self.mqtt_username = mqtt_username
+        self.mqtt_password = mqtt_password
+        self.update_interval = update_interval
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="EmberMqttBridge",
+        description="Integrate your Ember mug with your MQTT server")
+    
+    parser.add_argument("-c", "--config-file",
+        help="Path to a YAML file from which to read options.")
+    
+    parser.add_argument("--MAC",
+        help="MAC address of your Ember mug.")
+
+    parser.add_argument("-b", "--mqtt-broker",
+        help="Target MQTT broker, like test.mosquitto.org.")
+
+    parser.add_argument("-P", "--mqtt-broker-port", type=int, default=1883,
+        help="Target MQTT broker port, like 1883.")
+    
+    parser.add_argument("-u", "--mqtt-username",
+        help="Username to authenticate to the MQTT broker.")
+
+    parser.add_argument("-p", "--mqtt-password",
+        help="Password to authenticate to the MQTT broker.")
+
+    parser.add_argument("-i", "--update-interval", type=int, default=30,
+        help="Frequency at which to send out update messages, in seconds.")
+    
+    parser.add_argument
+
+    args = parser.parse_args()
+    config = {}
+
+    if args.config_file:
+        with open(args.config_file, "r", encoding="utf-8") as config_file:
+            config = yaml.safe_load(config_file)
+
+    for arg in vars(args):
+        config[arg] = getattr(args, arg)
+
+if __name__ == "__main__":
+    main()
